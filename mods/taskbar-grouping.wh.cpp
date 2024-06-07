89591a73c83e6630eb6bb4f884015435f74c65e7
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-06-07 10:56:29 +0300

    taskbar-grouping 1.3.3

--- mods/taskbar-grouping.wh.cpp
+++ mods/taskbar-grouping.wh.cpp
@@ -2,7 +2,7 @@
 // @id              taskbar-grouping
 // @name            Disable grouping on the taskbar
 // @description     Causes a separate button to be created on the taskbar for each new window
-// @version         1.3.2
+// @version         1.3.3
 // @author          m417z
 // @github          https://github.com/m417z
 // @twitter         https://twitter.com/m417z
