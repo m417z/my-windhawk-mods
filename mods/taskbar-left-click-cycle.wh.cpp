f690a1f008d5364a5ea916b82a70aae6ea7e5d08
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2024-10-22 12:08:04 +0300

    taskbar-left-click-cycle: Fix ExplorerPatcher initialization, don't require enabling the Win10 option

--- mods/taskbar-left-click-cycle.wh.cpp
+++ mods/taskbar-left-click-cycle.wh.cpp
@@ -214,6 +214,10 @@ bool HookExplorerPatcherSymbols(HMODULE explorerPatcherModule) {
         return true;
     }
 
+    if (g_winVersion >= WinVersion::Win11) {
+        g_winVersion = WinVersion::Win10;
+    }
+
     struct EXPLORER_PATCHER_HOOK {
         PCSTR symbol;
         void** pOriginalFunction;
@@ -366,18 +370,20 @@ BOOL Wh_ModInit() {
         if (hasWin10Taskbar && !HookTaskbarSymbols()) {
             return FALSE;
         }
-
-        HandleLoadedExplorerPatcher();
-
-        HMODULE kernelBaseModule = GetModuleHandle(L"kernelbase.dll");
-        FARPROC pKernelBaseLoadLibraryExW =
-            GetProcAddress(kernelBaseModule, "LoadLibraryExW");
-        Wh_SetFunctionHook((void*)pKernelBaseLoadLibraryExW,
-                           (void*)LoadLibraryExW_Hook,
-                           (void**)&LoadLibraryExW_Original);
     } else if (!HookTaskbarSymbols()) {
         return FALSE;
     }
+
+    HandleLoadedExplorerPatcher();
+
+    HMODULE kernelBaseModule = GetModuleHandle(L"kernelbase.dll");
+    FARPROC pKernelBaseLoadLibraryExW =
+        GetProcAddress(kernelBaseModule, "LoadLibraryExW");
+    Wh_SetFunctionHook((void*)pKernelBaseLoadLibraryExW,
+                       (void*)LoadLibraryExW_Hook,
+                       (void**)&LoadLibraryExW_Original);
+
+    g_initialized = true;
 
     return TRUE;
 }
@@ -387,7 +393,7 @@ void Wh_ModAfterInit() {
 
     // Try again in case there's a race between the previous attempt and the
     // LoadLibraryExW hook.
-    if (g_settings.oldTaskbarOnWin11 && !g_explorerPatcherInitialized) {
+    if (!g_explorerPatcherInitialized) {
         HandleLoadedExplorerPatcher();
     }
 }
