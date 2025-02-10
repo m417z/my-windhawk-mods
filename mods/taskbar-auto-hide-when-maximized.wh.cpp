4013efb06fccb6e1b2033f28dd4afdf2830bbf2e
Xịn Sò <xinso.dev@gmail.com>
2025-02-10 23:50:18 +0700

    Apply the auto-hide taskbar only for the focused window
    Add an option to apply auto-hide taskbar only for the focused window.

--- mods/taskbar-auto-hide-when-maximized.wh.cpp
+++ mods/taskbar-auto-hide-when-maximized.wh.cpp
@@ -42,6 +42,10 @@ or a similar tool), enable the relevant option in the mod's settings.
   - intersected: Auto-hide when a window is maximized or intersects the taskbar
   - maximized: Auto-hide only when a window is maximized
   - never: Never auto-hide
+- focusedWindow: true
+  $name: Apply only to focused window
+  $description: >-
+    Enable this option to apply the auto-hide taskbar feature only to the selected window.
 - oldTaskbarOnWin11: false
   $name: Customize the old taskbar on Windows 11
   $description: >-
@@ -69,6 +73,7 @@ enum class Mode {
 struct {
     Mode mode;
     bool oldTaskbarOnWin11;
+    bool focusedWindow;
 } g_settings;
 
 enum class WinVersion {
@@ -215,57 +220,67 @@ bool GetTaskbarRectForMonitor(HMONITOR monitor, RECT* rect) {
     return true;
 }
 
-bool ShouldAlwaysShowTaskbar(HWND hMMTaskbarWnd, HMONITOR monitor) {
+bool CanHideTaskbar(HWND hWnd, HMONITOR monitor) {
+    RECT taskbarRect{};
+    GetTaskbarRectForMonitor(monitor, &taskbarRect);
+
+    HWND hShellWindow = GetShellWindow();
+
+    if (hWnd == hShellWindow || GetProp(hWnd, L"DesktopWindow") ||
+        IsTaskbarWindow(hWnd) || !IsWindowVisible(hWnd) ||
+        IsWindowCloaked(hWnd) || IsIconic(hWnd)) {
+        return false;
+    }
+
+    if (GetWindowLong(hWnd, GWL_EXSTYLE) & WS_EX_NOACTIVATE) {
+        return false;
+    }
+
+    WINDOWPLACEMENT wp{
+        .length = sizeof(WINDOWPLACEMENT),
+    };
+
+    if (GetWindowPlacement(hWnd, &wp) && wp.showCmd == SW_SHOWMAXIMIZED) {
+        if (MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST) == monitor) {
+            return true;
+        }
+
+        return false;
+    }
+
+    if (pIsWindowArranged && pIsWindowArranged(hWnd) &&
+        MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST) != monitor) {
+        return false;
+    }
+
+    if (g_settings.mode == Mode::intersected) {
+        RECT rc;
+        RECT intersectRect;
+        if (GetWindowRect(hWnd, &rc) &&
+            IntersectRect(&intersectRect, &rc, &taskbarRect)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ShouldAlwaysShowTaskbar(HMONITOR monitor) {
     if (g_settings.mode == Mode::never) {
         return true;
     }
 
     bool canHideTaskbar = false;
 
-    RECT taskbarRect{};
-    GetTaskbarRectForMonitor(monitor, &taskbarRect);
-
-    HWND hShellWindow = GetShellWindow();
+    if (g_settings.focusedWindow) {
+        HWND hFocusedWnd = GetForegroundWindow();
+        canHideTaskbar = CanHideTaskbar(hFocusedWnd, monitor);
+        return !canHideTaskbar;
+    }
 
     auto enumWindowsProc = [&](HWND hWnd) -> BOOL {
-        if (hWnd == hShellWindow || GetProp(hWnd, L"DesktopWindow") ||
-            IsTaskbarWindow(hWnd) || !IsWindowVisible(hWnd) ||
-            IsWindowCloaked(hWnd) || IsIconic(hWnd)) {
-            return TRUE;
-        }
-
-        if (GetWindowLong(hWnd, GWL_EXSTYLE) & WS_EX_NOACTIVATE) {
-            return TRUE;
-        }
-
-        WINDOWPLACEMENT wp{
-            .length = sizeof(WINDOWPLACEMENT),
-        };
-        if (GetWindowPlacement(hWnd, &wp) && wp.showCmd == SW_SHOWMAXIMIZED) {
-            if (MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST) == monitor) {
-                canHideTaskbar = true;
-                return FALSE;
-            }
-
-            return TRUE;
-        }
-
-        if (pIsWindowArranged && pIsWindowArranged(hWnd) &&
-            MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST) != monitor) {
-            return TRUE;
-        }
-
-        if (g_settings.mode == Mode::intersected) {
-            RECT rc;
-            RECT intersectRect;
-            if (GetWindowRect(hWnd, &rc) &&
-                IntersectRect(&intersectRect, &rc, &taskbarRect)) {
-                canHideTaskbar = true;
-                return FALSE;
-            }
-        }
-
-        return TRUE;
+        canHideTaskbar = CanHideTaskbar(hWnd, monitor);
+        return !canHideTaskbar;
     };
 
     EnumWindows(
@@ -412,7 +427,7 @@ LRESULT WINAPI TrayUI_WndProc_Hook(void* pThis,
         }
     } else if (Msg == g_updateTaskbarStateRegisteredMsg) {
         HMONITOR monitor = TrayUI_GetStuckMonitor_Original(pThis);
-        bool alwaysShow = ShouldAlwaysShowTaskbar(hWnd, monitor);
+        bool alwaysShow = ShouldAlwaysShowTaskbar(monitor);
 
         void* pTrayUI_IInspectable =
             QueryViaVtableBackwards(pThis, TrayUI_vftable_IInspectable);
@@ -460,7 +475,7 @@ LRESULT WINAPI CSecondaryTray_v_WndProc_Hook(void* pThis,
         HMONITOR monitor =
             CSecondaryTray_GetMonitor_Original(pCSecondaryTray_ISecondaryTray);
 
-        bool alwaysShow = ShouldAlwaysShowTaskbar(hWnd, monitor);
+        bool alwaysShow = ShouldAlwaysShowTaskbar(monitor);
 
         bool alwaysShown = g_alwaysShowTaskbars.contains(pThis);
 
@@ -546,6 +561,13 @@ DWORD WINAPI WinEventHookThread(LPVOID lpThreadParameter) {
         Wh_Log(L"Error: SetWinEventHook");
     }
 
+    HWINEVENTHOOK winSystemEventHook1 =
+        SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, nullptr,
+                        WinEventProc, 0, 0, WINEVENT_OUTOFCONTEXT);
+    if (!winSystemEventHook1) {
+        Wh_Log(L"Error: SetWinEventHook");
+    }
+
     BOOL bRet;
     MSG msg;
     while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0) {
@@ -573,6 +595,10 @@ DWORD WINAPI WinEventHookThread(LPVOID lpThreadParameter) {
 
     if (winObjectEventHook3) {
         UnhookWinEvent(winObjectEventHook3);
+    }
+
+    if (winSystemEventHook1) {
+        UnhookWinEvent(winSystemEventHook1);
     }
 
     return 0;
@@ -850,6 +876,7 @@ void LoadSettings() {
     Wh_FreeStringSetting(mode);
 
     g_settings.oldTaskbarOnWin11 = Wh_GetIntSetting(L"oldTaskbarOnWin11");
+    g_settings.focusedWindow = Wh_GetIntSetting(L"focusedWindow");
 }
 
 BOOL Wh_ModInit() {
