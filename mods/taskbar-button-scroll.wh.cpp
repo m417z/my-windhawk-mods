d7a66ccb7b7da653ddbedf38dd7a17cd2fdc8e03
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-02-14 19:40:12 +0200

    taskbar-button-scroll 1.1.1

--- mods/taskbar-button-scroll.wh.cpp
+++ mods/taskbar-button-scroll.wh.cpp
@@ -2,7 +2,7 @@
 // @id              taskbar-button-scroll
 // @name            Taskbar minimize/restore on scroll
 // @description     Minimize/restore by scrolling the mouse wheel over taskbar buttons and thumbnail previews (Windows 11 only)
-// @version         1.1
+// @version         1.1.1
 // @author          m417z
 // @github          https://github.com/m417z
 // @twitter         https://twitter.com/m417z
