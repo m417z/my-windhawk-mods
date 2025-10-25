03d93dfbc8ca20c08236d6f3b821ae3635ab704e
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2025-10-25 17:16:59 +0300

    taskbar-icon-size 1.3.6

--- mods/taskbar-icon-size.wh.cpp
+++ mods/taskbar-icon-size.wh.cpp
@@ -2,7 +2,7 @@
 // @id              taskbar-icon-size
 // @name            Taskbar height and icon size
 // @description     Control the taskbar height and icon size, improve icon quality (Windows 11 only)
-// @version         1.3.5
+// @version         1.3.6
 // @author          m417z
 // @github          https://github.com/m417z
 // @twitter         https://twitter.com/m417z
