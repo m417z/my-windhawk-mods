1bac9c7ebf6bf8ea1e3488fb7011402dfb55db51
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2026-04-25 16:51:15 +0300

    taskbar-clock-customization 1.7.4

--- mods/taskbar-clock-customization.wh.cpp
+++ mods/taskbar-clock-customization.wh.cpp
@@ -2,7 +2,7 @@
 // @id              taskbar-clock-customization
 // @name            Taskbar Clock Customization
 // @description     Custom date/time format, news feed, weather, performance metrics (upload/download speed, CPU, RAM, GPU, battery), media player info, custom fonts and colors, and more
-// @version         1.7.3
+// @version         1.7.4
 // @author          m417z
 // @github          https://github.com/m417z
 // @twitter         https://twitter.com/m417z
@@ -691,7 +691,7 @@ winrt::event_token g_mediaPlaybackChangedToken;
 
 std::vector<std::optional<DYNAMIC_TIME_ZONE_INFORMATION>> g_timeZoneInformation;
 
-HANDLE g_webContentUpdateThread;
+std::atomic<HANDLE> g_webContentUpdateThread;
 HANDLE g_webContentUpdateRefreshEvent;
 HANDLE g_webContentUpdateStopEvent;
 std::mutex g_webContentMutex;
